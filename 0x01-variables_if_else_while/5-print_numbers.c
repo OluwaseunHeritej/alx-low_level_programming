b7fea84f394b0fd9a0eb4bbe1c045b00364a4e11
OluwaseunHeritej <heritejoluwaseun@gmail.com>
2022-04-10 17:44:22 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * variables
+ * Return: 0
+ */
+
+int main(void)
+{
+int n;
+while (n < 10)
+{
+rintf("%d", n);
+n += 1;
+}
+printf("\n");
+return (0);
+}
