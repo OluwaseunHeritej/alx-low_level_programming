d8b93aaa955fbd1fd13eac2e7052ea9f203f048a
OluwaseunHeritej <heritejoluwaseun@gmail.com>
2022-04-11 11:54:59 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * variables
+ * Return: 0
+ */
+
+int main(void)
+
+{
+int n = 48;
+int com = 44;
+while (n <= 57)
+
+{
+putchar(n);
+if (n != 57)
+
+{
+putchar(com);
+putchar(32);
+}
+
+n += 1;
+}
+
+putchar('\n');
+return (0);
+}
