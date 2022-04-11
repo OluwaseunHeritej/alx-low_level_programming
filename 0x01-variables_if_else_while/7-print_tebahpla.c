9d2da2467e6e2bd90852a0b020007f08f175f216
OluwaseunHeritej <heritejoluwaseun@gmail.com>
2022-04-11 11:29:51 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * variables
+ * Return: 0
+ */
+
+int main(void)
+
+{
+int n = 122;
+while (n > 96)
+
+{
+putchar(n);
+n -= 1;
+}
+
+putchar('\n');
+return (0);
+}
