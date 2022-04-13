221bb649dbefce7ba4242115aa1465b614579eca
OluwaseunHeritej <heritejoluwaseun@gmail.com>
2022-04-13 12:38:34 -0700

    Tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - Entry point
+ * @n: An int
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+
+if (n < 0)
+{
+return (n * (-1));
+}
+
+else if (n == 0)
+{
+return (0);
+}
+
+else
+{
+return (n);
+}
+
+}
