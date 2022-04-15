bb1f6901a3a6fbbf0fe135f5149dd096aae96fbc
OluwaseunHeritej <heritejoluwaseun@gmail.com>
2022-04-14 17:16:04 -0700

    Tasks

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ * @c: An int
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
