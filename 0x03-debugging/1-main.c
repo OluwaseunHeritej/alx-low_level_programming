c12e2f39bf499e7fafebcfc4b9552626149c45ef
OluwaseunHeritej <heritejoluwaseun@gmail.com>
2022-04-14 18:56:35 -0700

    Tasks

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+
+/**
+ * while (i < 10)
+ * {
+ * Infinite loop - No increment
+ * putchar(i);
+ * }
+ */
+
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
