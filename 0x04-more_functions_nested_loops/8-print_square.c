9e057539c2106353d7f368463d17fa799ced10d4
OluwaseunHeritej <heritejoluwaseun@gmail.com>
2022-04-14 18:14:19 -0700

    Tasks

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ * @size: an int
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+
+int i;
+int k;
+
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+
+for (k = 0; k < size; k++)
+{
+_putchar (35);
+}
+_putchar ('\n');
+
+}
+
+}
+
+else
+{
+_putchar ('\n');
+}
+
+}
