ac0e4338c45be867c3915fe80e53ff2136248312
OluwaseunHeritej <heritejoluwaseun@gmail.com>
2022-04-20 13:30:20 -0700

    Task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * swap_int - main entry
+ * @a: A pointer
+ * @b: A pointer
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int f;
+
+f = *a;
+*a = *b;
+*b = f;
+}
