3bb5e33024963b8159c9ac41c5ade38de41b49c5
OluwaseunHeritej <heritejoluwaseun@gmail.com>
2022-04-24 13:46:04 -0700

    Tasks

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - entry point
+ * @dest: A pointer
+ * @src: A pointer
+ * @n: value
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+
+}
