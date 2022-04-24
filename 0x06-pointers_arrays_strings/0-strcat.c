ab5ac00015a41b6d73708035277c7b8f429729de
OluwaseunHeritej <heritejoluwaseun@gmail.com>
2022-04-24 12:54:50 -0700

    Tasks

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - main entry
+ * @dest: A pointer
+ * @src: A pointer
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
